d2f64d1a61fd69ba77d50607c2d22ac64c2d37c2
sofiattr <kicatasofia@gmail.com>
2026-02-15 15:35:50 +0200

    Initial commit

---
+++ src/main/java/com/example/servletdemo/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.example.servletdemo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().write("<h1>Hello from HelloServlet!</h1>");
+    }
+}
